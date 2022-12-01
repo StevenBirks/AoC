5959b5d8abbc709600013a4b2c3b98c2e0098169
StevenBirks <34234552+StevenBirks@users.noreply.github.com>
2022-12-01 21:07:05 +0000

    day1

---
+++ 2022/2022-1-2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = ReadInput();
+
+            var result = Calculate(input);
+
+            Console.WriteLine($"Result: {result}");
+        }
+
+        private static string ReadInput() {
+            return System.IO.File.ReadAllText(@"./input.txt");
+        }
+
+        private static int Calculate(string input) {
+            var currentCount = 0;
+            var elfCals = new List<int>();
+
+            foreach (var i in input.Split("\r\n")) {
+                if (i == string.Empty) {
+                    elfCals.Add(currentCount);
+                    currentCount = 0;
+                } else {
+                    currentCount += int.Parse(i);
+                }
+            }
+
+            return elfCals.OrderByDescending(o => o).Take(3).Sum();
+        }
+    }
+}
---
+++ 2022/2022-2-1/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = ReadInput();
+
+            var parsedInput = ParseInput(input);
+
+            var result = Calculate(parsedInput);
+
+            Console.WriteLine($"Result: {result}");
+        }
+
+        private static int Calculate(List<Pwordandrule> parsedInput) {
+            var result = 0;
+
+            foreach (var item in parsedInput) {
+                var count = item.Pword.Count(o => o == item.Character);
+
+                if (count >= item.Min && count <= item.Max) {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+        private static string ReadInput() {
+            return System.IO.File.ReadAllText(@"./input.txt");
+        }
+
+        private static List<Pwordandrule> ParseInput(string input) {
+            var parsedInput = new List<Pwordandrule>();
+
+            foreach (var i in input.Split("\r\n")) {
+                var newVal = new Pwordandrule();
+
+                newVal.Min = int.Parse(i.Split(" ")[0].Split("-")[0]);
+                newVal.Max = int.Parse(i.Split(" ")[0].Split("-")[1]);
+                newVal.Character = char.Parse(i.Split(" ")[1].Remove(1));
+                newVal.Pword = i.Split(" ")[2];
+                parsedInput.Add(newVal);
+            }
+
+            return parsedInput;
+        }
+    }
+
+    class Pwordandrule {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public char Character { get; set; }
+        public string Pword { get; set; } = string.Empty;
+    }
+}
