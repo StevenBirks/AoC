4d8c95a6a7e13a8c5ab9650180656a58fa15502b
StevenBirks <34234552+StevenBirks@users.noreply.github.com>
2022-12-06 21:05:56 +0000

    day6

---
+++ 2022/2022-6-1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = ReadInput();
+
+            var result = Calculate(input);
+
+            Console.WriteLine($"Result: {result}");
+        }
+
+        private static int Calculate(string input) {
+            for (var i = 3; i < input.Length; i++) {
+                var count = input.Substring(i-3, 4).ToList().Distinct().Count();
+
+                if (count == 4) {
+                    return i+1;
+                }
+            }
+
+            return 0; // fail
+        }
+
+        private static string ReadInput() {
+            return System.IO.File.ReadAllText(@"./input.txt");
+        }
+    }
+}
---
+++ 2022/2022-6-2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = ReadInput();
+
+            var result = Calculate(input);
+
+            Console.WriteLine($"Result: {result}");
+        }
+
+        private static int Calculate(string input) {
+            for (var i = 13; i < input.Length; i++) {
+                var count = input.Substring(i-13, 14).ToList().Distinct().Count();
+
+                if (count == 14) {
+                    return i+1;
+                }
+            }
+
+            return 0; // fail
+        }
+
+        private static string ReadInput() {
+            return System.IO.File.ReadAllText(@"./input.txt");
+        }
+    }
+}
