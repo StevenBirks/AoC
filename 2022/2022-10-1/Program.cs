403f3cd8b9624ace20f0d20f48b99f437ef44c30
StevenBirks <34234552+StevenBirks@users.noreply.github.com>
2022-12-10 11:39:31 +0000

    day10

---
+++ 2022/2022-10-1/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        public static HashSet<int> MasterCycles = new HashSet<int>();
+        public static int Register = 1;
+        public static HashSet<int> RegisterValues = new HashSet<int>();
+
+        static void Main(string[] args)
+        {
+            var input = ReadInput();
+
+            var parsedInput = ParseInput(input);
+
+            InitMasterCycles();
+
+            var result = Calculate(parsedInput);
+
+            Console.WriteLine($"Result: {result}");
+        }
+
+        private static int Calculate(List<Operation> operations) {
+            var cycle = 1;
+            foreach (var op in operations) {
+                CheckCycle(cycle);
+                cycle++;
+
+                if (op.Type == OpType.addx) {
+                    CheckCycle(cycle);
+                    cycle++;
+
+                    Register += (int)op.Value;
+                }
+            }
+
+            return RegisterValues.ToList().Sum();
+        }
+
+        private static void CheckCycle(int cycle) {
+            if (MasterCycles.Contains(cycle)) {
+                RegisterValues.Add(Register * cycle);
+            }
+        }
+
+        private static void InitMasterCycles() {
+            MasterCycles.Add(20);
+            MasterCycles.Add(60);
+            MasterCycles.Add(100);
+            MasterCycles.Add(140);
+            MasterCycles.Add(180);
+            MasterCycles.Add(220);
+        }
+
+        private static List<Operation> ParseInput(string input) {
+            var operations = new List<Operation>();
+
+            foreach (var line in input.Split("\r\n")) {
+                var newOp = new Operation();
+
+                if (line.Split(" ")[0] == "noop") {
+                    newOp.Type = OpType.noop;
+                } else {
+                    newOp.Type = OpType.addx;
+                    newOp.Value = int.Parse(line.Split(" ")[1]);
+                }
+
+                operations.Add(newOp);
+            }
+
+            return operations;
+        }
+
+        private static string ReadInput() {
+            return System.IO.File.ReadAllText(@"./input.txt");
+        }
+    }
+
+    public class Operation {
+        public OpType Type { get; set; }
+        public int? Value { get; set; }
+    }
+
+    public enum OpType  {
+        addx,
+        noop
+    }
+}
---
+++ 2022/2022-10-2/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        public static HashSet<int> MasterCycles = new HashSet<int>();
+        public static int Register = 1;
+        public static List<List<char>> Rows = new List<List<char>>();
+
+        static void Main(string[] args)
+        {
+            var input = ReadInput();
+
+            var parsedInput = ParseInput(input);
+
+            Calculate(parsedInput);
+
+            Display();
+        }
+
+        private static void Display() {
+            foreach (var row in Rows) {
+                Console.WriteLine(new string(row.ToArray()));
+            }
+        }
+
+        private static void Calculate(List<Operation> operations) {
+            var cycle = 1;
+            foreach (var op in operations) {
+                Step(cycle);
+                cycle++;
+
+                if (op.Type == OpType.addx) {
+                    Step(cycle);
+                    cycle++;
+
+                    Register += (int)op.Value;
+                }
+            }
+        }
+
+        private static void Step(int cycle) {
+            var pos = (cycle - 1) % 40;
+
+            if (pos == 0) {
+                Rows.Add(new List<char>());
+            }
+
+            var displayChar = '.';
+
+            if (Math.Abs(Register - pos) <= 1) {
+                displayChar = '#';
+            }
+
+            Rows.Last().Add(displayChar);
+        }
+
+        private static List<Operation> ParseInput(string input) {
+            var operations = new List<Operation>();
+
+            foreach (var line in input.Split("\r\n")) {
+                var newOp = new Operation();
+
+                if (line.Split(" ")[0] == "noop") {
+                    newOp.Type = OpType.noop;
+                } else {
+                    newOp.Type = OpType.addx;
+                    newOp.Value = int.Parse(line.Split(" ")[1]);
+                }
+
+                operations.Add(newOp);
+            }
+
+            return operations;
+        }
+
+        private static string ReadInput() {
+            return System.IO.File.ReadAllText(@"./input.txt");
+        }
+    }
+
+    public class Operation {
+        public OpType Type { get; set; }
+        public int? Value { get; set; }
+    }
+
+    public enum OpType  {
+        addx,
+        noop
+    }
+}
